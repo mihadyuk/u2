c6ab9fecd3e21bd44f2d6a0d2041664bff47dda0
barthess <barthess@yandex.ru>
2014-08-28 18:37:26 +0300

    Nop

--- firmware/mtd_conf.h
+++ firmware/mtd_conf.h
@@ -4,6 +4,6 @@
 #include "pads.h"
 
 #define MTD_USE_MUTUAL_EXCLUSION            TRUE
-#define MTD_WRITE_BUF_SIZE                  (128 + 2)
+#define MTD_WRITE_BUF_SIZE                  (32 + 2)
 
 #endif /* MTD_CONF_H_ */
