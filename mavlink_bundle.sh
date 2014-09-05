dc1d73e009324505927b880a6e23c44c735a682d
barthess <barthess@yandex.ru>
2014-09-05 13:47:45 +0300

    Updated mavlink

--- mavlink_bundle.sh
+++ mavlink_bundle.sh
@@ -2,7 +2,7 @@
 
 cp firmware/lib/mavlink/definitions/lapwing.xml mavlink_generator/message_definitions/v1.0/
 cd mavlink_generator/pymavlink
-python setup.py bdist --formats=wininst --plat-name=win32
+PYTHONPATH=generator python setup.py bdist --formats=wininst --plat-name=win32
 mv dist/* ../..
 rm -rf dist
 rm -rf build
--- mavlink_install.sh
+++ mavlink_install.sh
@@ -2,7 +2,7 @@
 
 cp firmware/lib/mavlink/definitions/lapwing.xml mavlink_generator/message_definitions/v1.0/
 cd mavlink_generator/pymavlink
-python ./setup.py install --user
+PYTHONPATH=generator python ./setup.py install --user
+rm -rf build
 cd ../..
 rm mavlink_generator/message_definitions/v1.0/lapwing.xml
-
