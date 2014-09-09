795460d16fd7e28f23c2c15bd27d664ed8272e52
barthess <barthess@yandex.ru>
2014-09-09 18:17:24 +0300

    Nop

--- firmware/src/fault_handlers.h
+++ firmware/src/fault_handlers.h
@@ -8,19 +8,18 @@
 #define is_under_debugger()   (((CoreDebug)->DHCSR) & CoreDebug_DHCSR_C_DEBUGEN_Msk)
 
 /**
- * evaluates to TRUE if system boots after soft reset cause by SYSRESETREQ
+ * evaluates to TRUE if system boots after soft reset caused by SYSRESETREQ
  */
 #define was_softreset()       (((RCC)->CSR) & RCC_CSR_SFTRSTF)
 
 /**
- * evaluates to TRUE if system boots after reset by power pad
+ * evaluates to TRUE if system boots after reset by reset pad
  */
 #define was_padreset()        (((RCC)->CSR) & RCC_CSR_PADRSTF)
 
 /**
  * Determine need of full initialization procedure logic
  */
-#define NEED_FULL_INIT_MASK   (RCC_CSR_PADRSTF & RCC_CSR_SFTRSTF)
 //#define need_full_init()    (!(was_softreset() || was_padreset()))
 #define need_full_init()      (!(was_softreset()))
 
