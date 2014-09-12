be34986c7d787c76a56ddbdd8cb2395afeda79bd
barthess <barthess@yandex.ru>
2014-09-12 13:22:22 +0300

    Added draft tlm_sender

---
+++ firmware/src/mavlink_local/tlm_sender.hpp
@@ -0,0 +1,17 @@
+#ifndef TLM_SENDER_HPP_
+#define TLM_SENDER_HPP_
+
+class TlmSender {
+public:
+  TlmSender(void);
+  void start(void);
+  void pause(void);
+  void resume(void);
+  void stop(void);
+private:
+  Thread *worker;
+};
+
+void TlmSenderInit(void);
+
+#endif /* TLM_SENDER_HPP_ */
