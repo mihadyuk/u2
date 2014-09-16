db98078a71e31d604231453dae41bd730c49c5ee
barthess <barthess@yandex.ru>
2014-09-16 16:15:47 +0300

    nop

--- firmware/lib/uav_utils/crc.h
+++ firmware/lib/uav_utils/crc.h
@@ -1,7 +1,13 @@
-#ifndef CRC_HPP_
-#define CRC_HPP_
+#ifndef CRC_H_
+#define CRC_H_
 
-uint32_t crc32(const uint8_t *buf, size_t len);
-uint8_t crc8(const uint8_t *buf, size_t len);
+#ifdef __cplusplus
+extern "C" {
+#endif
+  uint32_t crc32(const uint8_t *buf, size_t len);
+  uint8_t crc8(const uint8_t *buf, size_t len);
+#ifdef __cplusplus
+}
+#endif
 
-#endif /* CRC_HPP_ */
+#endif /* CRC_H_ */
