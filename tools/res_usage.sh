522035c49fccf8f5928363b56bd04fc7eef83d46
barthess <barthess@yandex.ru>
2014-11-25 16:49:09 +0300

    Updated res_usage script

--- tools/res_usage.sh
+++ tools/res_usage.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-FLASH_SIZE=1048576
-FLASH_USED=`du -b build/ch.bin | gawk '{print$1}'`
+FLASH_SIZE=`cat build/ch.map | grep '^flash.*.[0..9]' | awk '{print$3}'`
+FLASH_USED=`arm-none-eabi-size -B build/ch.elf | tail -1 | awk '{print $1 + $2}'`
 
-RAM_SIZE=`cat build/ch.map | grep "__ram_size__ =" | gawk '{print$1}'`
-RAM_USED=`arm-none-eabi-size build/ch.elf | tail -1 | awk '{print $2+$3}'`
+RAM_SIZE=`cat build/ch.map | grep "__ram_size__ =" | awk '{print$1}'`
+STACKS=`arm-none-eabi-size -A build/ch.elf | grep .stacks | awk '{print $2}'`
+BSS=`arm-none-eabi-size -A build/ch.elf | grep .bss | awk '{print $2}'`
+DATA=`arm-none-eabi-size -A build/ch.elf | grep .data | awk '{print $2}'`
+let RAM_USED=$STACKS+$BSS+$DATA
 
-echo "------------------------------------------------------------------------------------------------------------"
+CCM_SIZE=`cat build/ch.map | grep "__ccmram_size__ =" | awk '{print$1}'`
+CCM_USED=`arm-none-eabi-size -A build/ch.elf | grep .ccm | awk '{print $2}'`
 
-let "FLASH_PERCENT	= (100 * FLASH_USED)/FLASH_SIZE"
+echo "---------------------------------------------------------------------------------------"
+
+let "FLASH_SIZE		= FLASH_SIZE" # to convert from hex to dec
 let "RAM_SIZE		= RAM_SIZE" # to convert from hex to dec
-let "RAM_PERCENT	= (100 * RAM_USED)/RAM_SIZE"
+let "CCM_SIZE       = CCM_SIZE" # to convert from hex to dec
+let "FLASH_PERCENT	= (100 * FLASH_USED) / FLASH_SIZE"
+let "RAM_PERCENT	= (100 * RAM_USED) / RAM_SIZE"
+let "CCM_PERCENT    = (100 * CCM_USED) / CCM_SIZE"
 
-echo "SRAM:  used = $RAM_USED, mcu_ram = $RAM_SIZE, utilization = $RAM_PERCENT%"
-echo "FLASH: program = $FLASH_USED, mcu_flash = $FLASH_SIZE, utilization = $FLASH_PERCENT%"
+echo "SRAM:  $RAM_USED / $RAM_SIZE ($RAM_PERCENT%)"
+echo "CCM:   $CCM_USED / $CCM_SIZE ($CCM_PERCENT%)"
+echo "FLASH: $FLASH_USED / $FLASH_SIZE ($FLASH_PERCENT%)"
