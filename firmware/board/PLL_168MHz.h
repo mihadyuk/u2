c8dae6cea8159e0cbc615901e710fdba83d41831
barthess <barthess@yandex.ru>
2014-11-25 16:52:03 +0300

    Added PLL settings files

---
+++ firmware/board/PLL_168MHz.h
@@ -0,0 +1,4 @@
+#define STM32_PLLM_VALUE                    8
+#define STM32_PLLN_VALUE                    336
+#define STM32_PLLP_VALUE                    2
+#define STM32_PLLQ_VALUE                    7
---
+++ firmware/board/PLL_32MHz.h
@@ -0,0 +1,4 @@
+#define STM32_PLLM_VALUE                    8
+#define STM32_PLLN_VALUE                    64
+#define STM32_PLLP_VALUE                    2
+#define STM32_PLLQ_VALUE                    2
