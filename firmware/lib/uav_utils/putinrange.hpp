c54d27ecb26a8a66be9c954452aeb57accd4a891
barthess <barthess@yandex.ru>
2014-12-22 16:05:45 +0300

    nop

--- firmware/lib/uav_utils/putinrange.hpp
+++ firmware/lib/uav_utils/putinrange.hpp
@@ -3,7 +3,7 @@
 
 #include <algorithm>
 
-#include "min_max.hpp"
+//#include "min_max.hpp"
 
 /**
  * Clamper function implementation
@@ -11,7 +11,7 @@
 template<typename T>
 T __putinrange_impl(const T& v, const T& lower, const T& upper) {
 
-  if(lower <= upper)
+  if (lower <= upper)
     return std::max(lower, std::min(v, upper));
   else /* protection from stupidity */
     return std::min(lower, std::max(v, upper));
@@ -22,9 +22,7 @@ T __putinrange_impl(const T& v, const T& lower, const T& upper) {
  */
 template<typename T, typename T2, typename T3>
 T putinrange(const T& v, const T2& lower, const T3& upper) {
-  return __putinrange_impl(v,
-        static_cast<typeof(v)>(lower),
-        static_cast<typeof(v)>(upper));
+  return __putinrange_impl(v, static_cast<T>(lower), static_cast<T>(upper));
 }
 
 #endif /* PUTINRANGE_HPP_ */
