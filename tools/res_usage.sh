98653ef55b106749cc7e1257e902861738d17988
Andrey Mihadyuk <mihadyuk@ntlab.com>
2015-04-08 14:24:42 +0300

    tools: Fixed res usage.

--- tools/res_usage.sh
+++ tools/res_usage.sh
@@ -16,6 +16,7 @@ let RAM_USED=$STACKS+$BSS+$DATA
 CCM_ALIAS=ram4
 CCM_SIZE=`cat build/ch.map | grep "__${CCM_ALIAS}_size__ =" | awk '{print$1}'`
 CCM_USED=`arm-none-eabi-size -A build/ch.elf | grep "."${CCM_ALIAS} | awk '{print $2}'`
+let "CCM_SIZE       = CCM_SIZE" # to convert from hex to dec
 
 echo "---------------------------------------------------------------------------------------"
 
@@ -27,8 +28,7 @@ let "FLASH_SIZE		= FLASH_SIZE" # to convert from hex to dec
 let "FLASH_PERCENT	= (100 * FLASH_USED) / FLASH_SIZE"
 echo "FLASH: $FLASH_USED / $FLASH_SIZE ($FLASH_PERCENT%)"
 
-if [ $CCM_SIZE ] ; then
-	let "CCM_SIZE       = CCM_SIZE" # to convert from hex to dec
+if [ $CCM_SIZE != 0 ] ; then
 	let "CCM_PERCENT    = (100 * CCM_USED) / CCM_SIZE"
 	echo "CCM:   $CCM_USED / $CCM_SIZE ($CCM_PERCENT%)"
 fi
