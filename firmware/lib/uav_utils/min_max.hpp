56995487efd4cdab622e624529928ceae035191c
barthess <barthess@yandex.ru>
2015-06-25 16:58:05 +0300

    Lib. Added special constexpr functions 'min' and 'max'

--- firmware/lib/uav_utils/min_max.hpp
+++ firmware/lib/uav_utils/min_max.hpp
@@ -13,6 +13,16 @@ T min(T v1, T v2) {
     return v2;
 }
 
+template<typename T>
+T min(T v1, T v2, T v3) {
+  return min(min(v1, v2), v3);
+}
+
+template<typename T>
+T min(T v1, T v2, T v3, T v4) {
+  return min(min(v1, v2, v3), v4);
+}
+
 /**
  *
  */
@@ -23,6 +33,52 @@ T max(T v1, T v2) {
     return v1;
   else
     return v2;
+}
+
+template<typename T>
+T max(T v1, T v2, T v3) {
+  return max(max(v1, v2), v3);
+}
+
+template<typename T>
+T max(T v1, T v2, T v3, T v4) {
+  return max(max(v1, v2, v3), v4);
+}
+
+/**
+ *
+ */
+template<typename T>
+constexpr T max_const(T v1, T v2) {
+  return ((v1 > v2) ? v1 : v2);
+}
+
+template<typename T>
+constexpr T max_const(T v1, T v2, T v3) {
+  return max_const(max_const(v1, v2), v3);
+}
+
+template<typename T>
+constexpr T max_const(T v1, T v2, T v3, T v4) {
+  return max_const(max_const(v1, v2, v3), v4);
+}
+
+/**
+ *
+ */
+template<typename T>
+constexpr T min_const(T v1, T v2) {
+  return ((v1 < v2) ? v1 : v2);
+}
+
+template<typename T>
+constexpr T min_const(T v1, T v2, T v3) {
+  return min_const(min_const(v1, v2), v3);
+}
+
+template<typename T>
+constexpr T min_const(T v1, T v2, T v3, T v4) {
+  return min_const(min_const(v1, v2, v3), v4);
 }
 
 #endif /* MIN_MAX_HPP_ */
