735451bf48bc932cb3fdab81601c5f675ff9f496
barthess <barthess@yandex.ru>
2015-11-12 15:09:11 +0300

    PWM channels redirection for FPGA
    Added yet another #ifdef for yet another board variant.

--- firmware/src/control/drivetrain/pwm_base.hpp
+++ firmware/src/control/drivetrain/pwm_base.hpp
@@ -8,6 +8,7 @@ namespace control {
 /**
  *
  */
+#if defined(BOARD_BEZVODIATEL)
 typedef enum {
   PWM_CH_AIL = 0,
   PWM_CH_ELE = 1,
@@ -15,6 +16,17 @@ typedef enum {
   PWM_CH_THR = 3,
   PWM_CH_THR_REVERSE = 0, // for simple PWM to brushed motor
 } pwm_ch_t;
+#elif defined(BOARD_MNU)
+typedef enum {
+  PWM_CH_AIL = 2,
+  PWM_CH_ELE = 3,
+  PWM_CH_RUD = 0,
+  PWM_CH_THR = 1,
+  PWM_CH_THR_REVERSE = 0, // for simple PWM to brushed motor
+} pwm_ch_t;
+#else
+#error "board unsupported"
+#endif
 
 /**
  *
