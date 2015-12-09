f731915e1765ace61dd2d95c1fcf87458f1848a3
barthess <barthess@yandex.ru>
2015-12-09 13:47:00 +0300

    trash cleanup

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -15,14 +15,9 @@ Giovanni
 */
 
 // TODO: save DCM in bkp for faster startup after panic recovery
-// TODO: params in bkp
-// TODO: gyro update period in bkp
 // TODO: save mission data in bkp for recovery if panic occured during mission
 
-// TODO: cli for format, ls, rm
-
 // TODO: rewrite stab code in general case using aviation formulae.
-// TODO: probably migrate from float32 to double in coordinate calculations.
 
 // TODO: correct STOP handling in waypoint algorithm (incorrect realization in QGC)
 
@@ -31,7 +26,6 @@ Giovanni
 // TODO: Power brown out handler (using ADC comparator on power supply pin?) for sync/umout SDC.
 // TODO: One more point in dynamic pressure thermal compensation algorithm (at +60 celsius)
 // TODO: Rewrite XBee code for use DMA.
-// TODO: WDT?
 
 #include "main.h"
 
