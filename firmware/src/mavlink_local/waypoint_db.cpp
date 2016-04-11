1d7a12f4d3daf17afb2cf424f0ec69aadac153eb
barthess <barthess@yandex.ru>
2016-04-11 14:43:34 +0300

    Improved mission reseiving/storing
    Now storage splitten in 2 independant banks switching
    only in case of successful write.

--- firmware/src/mavlink_local/waypoint_db.cpp
+++ firmware/src/mavlink_local/waypoint_db.cpp
@@ -5,6 +5,15 @@
 #include "soft_crc.h"
 
 /*
+ * 1) uint16_t. Заголовок, он же количество точек в текущем маршруте
+ * 2) Старший бит заголовка означает нормер банка (0, 1)
+ * 3) Остальные биты заголовка кодируют количество точек активного банка
+ * 4) uint8_t[WAYPOINT_FOOTPRINT]. 2 банка данных. Теневой банк служит
+ *    для приема входящих точек маршрута. В случае успешной загрузки
+ *    полетного задания теневой банк становится активным.
+ */
+
+/*
  ******************************************************************************
  * DEFINES
  ******************************************************************************
@@ -43,6 +52,34 @@ static uint8_t buf[WAYPOINT_FOOTPRINT];
  */
 
 /*
+ *
+ */
+static void seal_with_crc(uint8_t *buf) {
+  buf[WAYPOINT_FOOTPRINT - 1] = crc8(buf, WAYPOINT_FOOTPRINT - 1, 0xFF);
+}
+
+/*
+ *
+ */
+static bool crc_valid(const uint8_t *buf) {
+  return buf[WAYPOINT_FOOTPRINT - 1] == crc8(buf, WAYPOINT_FOOTPRINT - 1, 0xFF);
+}
+
+/*
+ *
+ */
+static size_t next_bank(size_t current) {
+  return (current + 1) & 1;
+}
+
+/**
+ *
+ */
+size_t WpDB::calc_offset(uint16_t seq, size_t bank) {
+  return HEADER_SIZE + seq * WAYPOINT_FOOTPRINT + bank * WAYPOINT_FOOTPRINT * this->capacity;
+}
+
+/*
  ******************************************************************************
  * EXPORTED FUNCTIONS
  ******************************************************************************
@@ -59,25 +96,37 @@ WpDB::WpDB(void) {
  */
 uint16_t WpDB::start(void) {
 
-  size_t offset, readcnt;
-  uint8_t crc;
+  size_t readcnt;
 
   dbfile = NvramTryOpen(WPDB_FILE_NAME, BOOTSTRAP_WPDB_FILE_SIZE);
   osalDbgCheck(nullptr != dbfile);
 
+  /* precalculate capacity for single bank */
+  this->capacity = (dbfile->getSize() - HEADER_SIZE) / (WAYPOINT_FOOTPRINT  * 2);
+
   dbfile->setPosition(0);
   dbfile->get(&count);
+  this->shadow_count = 0;
 
+  /* deduce what bank contains valid data and adjust 'count' when needed */
+  if (0 == (count & (1U << 15))){
+    active_bank = 0;
+  }
+  else {
+    active_bank = 1;
+    count &= ~(1U << 15);
+  }
+
+  /* validate data in current bank */
   for (size_t seq=0; seq<count; seq++) {
-    offset = HEADER_SIZE + seq * WAYPOINT_FOOTPRINT;
-    dbfile->setPosition(offset);
+    dbfile->setPosition(calc_offset(seq, active_bank));
     readcnt = dbfile->read(buf, WAYPOINT_FOOTPRINT);
     if (WAYPOINT_FOOTPRINT != readcnt)
       goto FAILED;
 
-    crc = crc8(buf, WAYPOINT_FOOTPRINT - 1, 0xFF);
-    if (buf[WAYPOINT_FOOTPRINT - 1] != crc)
+    if (! crc_valid(buf)) {
       goto FAILED;
+    }
   }
   return count;
 
@@ -93,6 +142,7 @@ void WpDB::stop(void) {
 
   nvram_fs.close(this->dbfile);
   this->dbfile = nullptr;
+  count = 0;
 }
 
 /**
@@ -104,43 +154,57 @@ bool WpDB::read(mavlink_mission_item_t *wpp, uint16_t seq) {
 
   osalDbgCheck(nullptr != this->dbfile);
   osalDbgCheck(nullptr != wpp);
+  osalDbgCheck(active_bank <= 1);
 
   if (seq >= count)
     return OSAL_FAILED;
   else {
-    size_t offset = HEADER_SIZE + seq * WAYPOINT_FOOTPRINT;
-    dbfile->setPosition(offset);
-    result = dbfile->read((uint8_t *)wpp, sizeof(*wpp));
-    if (sizeof(*wpp) == result)
-      return OSAL_SUCCESS;
-    else
+    dbfile->setPosition(calc_offset(seq, active_bank));
+
+    result = dbfile->read(buf, WAYPOINT_FOOTPRINT);
+    if (WAYPOINT_FOOTPRINT != result)
       return OSAL_FAILED;
+
+    if (! crc_valid(buf))
+      return OSAL_FAILED;
+
+    memcpy(wpp, buf, sizeof(*wpp));
+    return OSAL_SUCCESS;
   }
 }
 
 /**
- *
+ * @brief   Write always performes to shadow bank
  */
 bool WpDB::write(const mavlink_mission_item_t *wpp, uint16_t seq) {
 
-  size_t written = 0;
-  uint8_t crc;
+  size_t bytecnt = 0;
+  const size_t shadow_bank = next_bank(active_bank);
 
   chDbgCheck(nullptr != this->dbfile);
   chDbgCheck(nullptr != wpp);
 
-  dbfile->setPosition(HEADER_SIZE + seq * WAYPOINT_FOOTPRINT);
-  written = dbfile->write((uint8_t *)wpp, sizeof(*wpp));
-  if (sizeof(*wpp) != written)
+  memcpy(buf, wpp, sizeof(*wpp));
+  seal_with_crc(buf);
+
+  /* actual write */
+  dbfile->setPosition(calc_offset(seq, shadow_bank));
+  bytecnt = dbfile->write(buf, WAYPOINT_FOOTPRINT);
+  if (WAYPOINT_FOOTPRINT != bytecnt)
     return OSAL_FAILED;
 
-  crc = crc8((uint8_t *)wpp, WAYPOINT_FOOTPRINT - 1, 0xFF);
-  written = dbfile->write(&crc, sizeof(crc));
-  if (sizeof(crc) != written)
+  /* read back and verify checksum */
+  dbfile->setPosition(calc_offset(seq, shadow_bank));
+  bytecnt = dbfile->read(buf, WAYPOINT_FOOTPRINT);
+  if (WAYPOINT_FOOTPRINT != bytecnt)
     return OSAL_FAILED;
-  else {
-    count++;
+
+  if (crc_valid(buf)) {
+    shadow_count++;
     return OSAL_SUCCESS;
+  }
+  else {
+    return OSAL_FAILED;
   }
 }
 
@@ -155,29 +219,42 @@ uint16_t WpDB::getCount(void) const {
  *
  */
 uint16_t WpDB::getCapacity(void)  const {
-  return (dbfile->getSize() - HEADER_SIZE) / WAYPOINT_FOOTPRINT;
+  return capacity;
 }
 
 /**
- *
+ * @brief   Write waypoint count in new mission and switch active bank.
  */
-bool WpDB::seal(void){
+bool WpDB::seal(void) {
+
+  uint16_t cnt = shadow_count;
+  if (0 == active_bank) {
+    cnt |= 1U << 15;
+  }
 
   dbfile->setPosition(0);
-  size_t result = dbfile->put(count);
+  size_t result = dbfile->put(cnt);
 
-  if (HEADER_SIZE == result)
+  if (HEADER_SIZE == result) {
+    count = shadow_count;
+    active_bank = next_bank(active_bank);
+    shadow_count = 0;
     return OSAL_SUCCESS;
-  else
+  }
+  else {
     return OSAL_FAILED;
+  }
 }
 
 /**
- * Fast clear without data erasing
+ * Fast mission clear without data erasing
  */
 bool WpDB::reset(void) {
+
   size_t result = 0;
   count = 0;
+  shadow_count = 0;
+  active_bank = 0;
   dbfile->setPosition(0);
   result = dbfile->put((uint16_t)0);
   if (HEADER_SIZE == result)
