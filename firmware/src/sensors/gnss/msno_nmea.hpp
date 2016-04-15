cf61f57b4100932555ecd43f15aa51c291335c1e
barthess <barthess@yandex.ru>
2016-04-15 12:14:45 +0300

    Safety improvements

--- firmware/src/sensors/gnss/msno_nmea.hpp
+++ firmware/src/sensors/gnss/msno_nmea.hpp
@@ -14,9 +14,9 @@ public:
   msnonmea(SerialDriver *sdp, uint32_t start_baudrate,
                               uint32_t working_baudrate);
 private:
-  void inject(void);
-  void subscribe_inject(void);
-  void release_inject(void);
+  void inject(void) override;
+  void subscribe_inject(void) override;
+  void release_inject(void) override;
 
   mavlink_message_t *recv_msg = nullptr;
   chibios_rt::Mailbox<mavlink_message_t*, 1> inject_mailbox;
