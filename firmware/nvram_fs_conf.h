83a60292f8dfa5d12055abc153e267a875f0ba2e
barthess <barthess@yandex.ru>
2016-04-20 19:07:32 +0300

    Updated 24aa submodule

---
+++ firmware/nvram_fs_conf.h
@@ -0,0 +1,12 @@
+#ifndef NVRAM_FS_CONF_H_
+#define NVRAM_FS_CONF_H_
+
+#if !defined(NVRAM_FS_MAX_FILE_NAME_LEN)
+#define NVRAM_FS_MAX_FILE_NAME_LEN        8
+#endif
+
+#if !defined(NVRAM_FS_MAX_FILE_CNT)
+#define NVRAM_FS_MAX_FILE_CNT             3
+#endif
+
+#endif /* NVRAM_FS_CONF_H_ */
